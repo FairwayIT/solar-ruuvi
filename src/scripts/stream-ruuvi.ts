ba64d710ef7e561e6549e1b5fe452563171a67a1
Marcus Robst <20040193+mrobst@users.noreply.github.com>
2021-12-23 14:16:26 +0000

    working code at last

--- src/scripts/stream-ruuvi.ts
+++ src/scripts/stream-ruuvi.ts
@@ -1,13 +1,10 @@
 import mqtt from 'async-mqtt';
 import EventEmitter from 'events';
 import ruuvi from 'node-ruuvitag';
-import { fromEvent, from, Observable, merge, debounceTime } from 'rxjs';
+import { fromEvent, Observable, auditTime } from 'rxjs';
 
 const client = mqtt.connect('mqtt://localhost');
 //console.log('client is', client);
-
-// let tag: EventEmitter;
-let tag$: Observable<any>;
 
 // connect to mqtt broker and publish a message
 client.on('connect', async function () {
@@ -23,95 +20,62 @@ client.on('connect', async function () {
   // });
 });
 
-let tags: EventEmitter[] = [];
-let message$: Observable<any>;
+// IIFE to use await but not at top level
+void (async function () {
+  const t = await ruuvi.findTags();
+  console.log('tags in IIFE are ', t);
 
-ruuvi.findTags().then((foundTags: EventEmitter[]) => {
-  tags = foundTags;
-  console.log('found tags', tags);
-  // });
+  // setup observables from first found tag updated event
+  const readings$: Observable<any> = fromEvent(t[0], 'updated');
+  const warnings$: Observable<any> = fromEvent(t[0], 'warning');
 
-  if (tags.length > 0) {
-    // create observable for the tag updated event emitter
-    message$ = fromEvent(tags[0], 'updated');
-    tag$ = new Observable((observer: any) => {
-      tags[0].on('updated', (val: any) => observer.next(val));
-      tags[0].on('error', (err: any) => observer.error(err));
-    });
-  }
-  // });
+  // take a reading every time period (don't use debounceTime!!!!!!)
+  const timedReadings$ = readings$.pipe(auditTime(60000));
 
-  const message2$ = fromEvent(tags[0], 'updated');
-  const error$ = fromEvent(tags[0], 'error');
-
-  // message$.subscribe({
-  //   next: (val) => console.log('val from message is ', val),
-  // });
-  message$.pipe(debounceTime(60000)).subscribe({
-    next: (val) => {
-      console.log('val from message2 with delay is ', val);
+  timedReadings$.subscribe({
+    next: async (reading) => {
+      // console.log('Reading from ruuvi tag with delay is ', reading);
       // publish the reading as JSON to MQTT
-      const reading = JSON.stringify(val, null, '\t');
-      // console.log('Got data from RuuviTag ' + tags[0].id + ':\n' + reading);
-      client.publish('solar/edge001', reading);
+      const JSONreading = JSON.stringify(reading, null, '\t');
+      console.log(
+        'Got data with delay from RuuviTag ' + t[0].id + ':\n' + reading
+      );
+      // push the reading into mqtt
+      const published = await client.publish('solar/edge001', JSONreading);
+      console.log('published response ', published);
+    },
+    error: (err) => {
+      console.error('issue with getting readings ', err);
     },
   });
-  error$.subscribe({
-    next: (val) => console.log('val from error is ', val),
+
+  // capture any warnings from the tag (or errors from the warnings!)
+  warnings$.subscribe({
+    next: (warning) => {
+      console.error(new Error(warning));
+    },
+    error: (err) => {
+      console.error(new Error(err));
+    },
   });
 
-  // const obs$ = merge(
-  //   message$.catch((err) => of(err)),
-  //   error$.mergeMap((val) => throw(val))
-  // );
-  // make observables from the event emitters
-  // const ruuvi$ = Observable.create((observer: any) => {
-  //   ruuvi.on('found', (val: any) => observer.next(val));
-  //   ruuvi.on('error', (err: any) => observer.error(err));
-  // });
+  // check the event emitter is emitting
+  t[0].on('updated', (data: any) => {
+    console.log('received data from updated event ');
+  });
+})(); // IIFE
 
-  // ruuvi$.subscribe({
-  //   next: (tag: EventEmitter) => {
-  //     console.log('ruuvi observable subscription val is ', tag);
-  //     // create observable for the tag updated event emitter
-  //     tag$ = new Observable((observer: any) => {
-  //       tag.on('updated', (val: any) => observer.next(val));
-  //       tag.on('error', (err: any) => observer.error(err));
-  //     });
+//   // // event callback based logic
+//   // // read the tag and publish to mqtt channel each reading
+//   // ruuvi.on('found', (tag: any) => {
+//   //   console.log('Found RuuviTag, id: ' + tag.id);
+//   //   tag.on('updated', (data: any) => {
+//   //     const reading = JSON.stringify(data, null, '\t');
+//   //     console.log('Got data from RuuviTag ' + tag.id + ':\n' + reading);
+//   //     client.publish('solar/edge002', reading);
+//   //   });
+//   // });
 
-  // subscribe to the observable
-  // if (tag$) {
-  // tag$.subscribe({
-  //   next: (val) => {
-  //     console.log('tag observable subscription val is ', val);
-  //   },
-  //   error: (err) => {
-  //     console.error('tag observable error is ', err);
-  //   },
-  // });
-  // }
-  //   },
-  //   error: (err: any) => {
-  //     console.error('ruuvi observable error is ', err);
-  //   },
-  // });
-  // }
-
-  // tags[0].on('updated', (data: any) => {
-  //   console.log('received data from updated event is ', data);
-  // });
-
-  // // event callback based logic
-  // // read the tag and publish to mqtt channel each reading
-  // ruuvi.on('found', (tag: any) => {
-  //   console.log('Found RuuviTag, id: ' + tag.id);
-  //   tag.on('updated', (data: any) => {
-  //     const reading = JSON.stringify(data, null, '\t');
-  //     console.log('Got data from RuuviTag ' + tag.id + ':\n' + reading);
-  //     client.publish('solar/edge002', reading);
-  //   });
-  // });
-
-  // ruuvi.on('warning', (message: any) => {
-  //   console.error(new Error(message));
-});
+//   // ruuvi.on('warning', (message: any) => {
+//   //   console.error(new Error(message));
+// });
